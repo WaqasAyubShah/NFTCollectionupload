90bbd652adfd1292f13697474f0b0b8c13563b5f
Abdulmoiz Ahmer <abdulmoizahmer1996@gmail.com>
2022-02-20 07:06:48 -0700

    Update nft.sol

--- nft.sol
+++ nft.sol
@@ -5,16 +5,16 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721Enumerable, Ownable {
+contract NftCollection is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string public baseURI;
+  string public baseImageURI;
   string public baseExtension = ".json";
-  string public imageURI;
   string public baseImageExtension= ".png";
   uint256 public cost = 0.01 ether;
   uint256 public maxSupply = 50;
-  uint256 public maxMintAmount = 10;
+  uint256 public maxMintAmount = 50;
   bool public paused = false;
   mapping(address => bool) public whitelisted;
 
@@ -26,14 +26,34 @@ contract NFT is ERC721Enumerable, Ownable {
   ) ERC721(_name, _symbol) {
     setBaseImageURI(_initImageURI);
     setBaseURI(_initBaseURI);
-    mint(msg.sender, 20);
+    mint(msg.sender, 50);
   }
 
-  function _baseimageURI() internal view virtual returns (string memory) {
-    return imageURI;
+  function setCost(uint256 _newCost) public onlyOwner() {
+    cost = _newCost;
   }
 
-  function timageURI(uint256 tokenId)
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
+    maxMintAmount = _newmaxMintAmount;
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseImageURI(string memory _newImageBaseURI) public onlyOwner {
+    baseImageURI = _newImageBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+
+  function getBaseImageURI() internal view virtual returns (string memory) {
+    return baseImageURI;
+  }
+
+  function getImageUriBasedOnToken(uint256 tokenId)
     public
     view
     virtual
@@ -44,20 +64,16 @@ contract NFT is ERC721Enumerable, Ownable {
       "ERC721Metadata: URI query for nonexistent token image"
     );
 
-    string memory currentBaseImageURI = _baseimageURI();
+    string memory currentBaseImageURI = getBaseImageURI();
     return bytes(currentBaseImageURI).length > 0
         ? string(abi.encodePacked(currentBaseImageURI, tokenId.toString(), baseImageExtension))
         : "";
   }
 
-  // internal
   function _baseURI() internal view virtual override returns (string memory) {
     return baseURI;
   }
 
-
-
-  // public
   function mint(address _to, uint256 _mintAmount) public payable {
     uint256 supply = totalSupply();
     require(!paused);
@@ -76,7 +92,7 @@ contract NFT is ERC721Enumerable, Ownable {
     }
   }
 
-  function walletOfOwner(address _owner)
+  function getAllNftTokensFromWalletOfOwner(address _owner)
     public
     view
     returns (uint256[] memory)
@@ -88,16 +104,6 @@ contract NFT is ERC721Enumerable, Ownable {
     }
     return tokenIds;
   }
-
-//   function tokenImageURI(address _owner) public view returns(string memory){
-//        uint256 ownerTokenCount = balanceOf(_owner);
-//     uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-//     for (uint256 i; i < ownerTokenCount; i++) {
-//       tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
-//     }
-//       return baseImageURI;
-//   }
-
 
   function tokenURI(uint256 tokenId)
     public
@@ -115,27 +121,6 @@ contract NFT is ERC721Enumerable, Ownable {
     return bytes(currentBaseURI).length > 0
         ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
         : "";
-  }
-
-  //only owner
-  function setCost(uint256 _newCost) public onlyOwner() {
-    cost = _newCost;
-  }
-
-  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
-    maxMintAmount = _newmaxMintAmount;
-  }
-
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
-  }
-
-  function setBaseImageURI(string memory _newBaseURI) public onlyOwner {
-    imageURI = _newBaseURI;
-  }
-
-  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-    baseExtension = _newBaseExtension;
   }
 
   function pause(bool _state) public onlyOwner {
